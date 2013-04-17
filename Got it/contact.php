aeed153bf3ab17c02177d4e6de317c2d743159b6
balcorta <alcorta.bruno@gmail.com>
2013-04-17 00:32:25 -0300

    Adding new content and logo

--- Got it/contact.php
+++ Got it/contact.php
@@ -1,7 +1,7 @@
 <?php
 
 $EmailFrom = $_REQUEST['email']; 
-$EmailTo = "@"; // Your email address here
+$EmailTo = "alcorta.bruno@gmail.com"; // Your email address here
 $Subject = "Contact form";
 $Name = Trim(stripslashes($_POST['name'])); 
 $Email = Trim(stripslashes($_POST['email'])); 
@@ -30,12 +30,183 @@ $Body .= "\n";
 
 // send email 
 $success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
-
-// redirect to success page 
-if ($success){
-  echo "Succes";
-}
-else{
-  echo "Error";
-}
 ?>
+	<!DOCTYPE html>
+
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+
+<head>
+	<meta charset="UTF-8">
+	
+	<!-- Remove this line if you use the .htaccess -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<meta name="viewport" content="width=device-width">
+	
+	<meta name="description" content="Designa Studio, a HTML5 / CSS3 template.">
+	<meta name="author" content="Sylvain Lafitte, Web Designer, sylvainlafitte.com">
+	
+	<title>CONTACT// GOTASOFT</title>
+	
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+	<link rel="shortcut icon" type="image/png" href="favicon.png">
+	
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/style.css">
+	
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+<!-- Prompt IE 7 users to install Chrome Frame -->
+<!--[if lt IE 8]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
+
+<div class="container">
+
+	<header id="navtop">
+		<a href="index.html" class="logo fleft">
+			<img src="img/logo.png" alt="Designa Studio">
+		</a>
+		
+		<nav class="fright">
+			<ul>
+				<li><a href="index.html">Home</a></li>				
+			</ul>
+			<ul>
+				<li><a href="about.html">About</a></li>
+			</ul>
+			<ul>				
+				<li><a href="contact.html" class="navactive">Contact</a></li>
+			</ul>
+		</nav>
+	</header>
+
+
+	<div class="contact-page main grid-wrap">
+
+		<header class="grid col-full">
+			<hr>
+			<p class="fleft">Contact</p>
+		</header>
+
+		
+		
+		<aside class="grid col-one-quarter mq2-col-one-third mq3-col-full">
+
+			<p class="mbottom">We would love to talk to you and answer all of your questions</p>
+			
+			<address class="mbottom">
+				<h5>GOTASOFT </h5>
+				1480 18 de Julio <br >
+				Level 4, Door 01 <br >
+				11200 MONTEVIDEO<br >
+				<a href="http://goo.gl/maps/JnTXS">Get directions</a>
+			</address>
+		
+			<!--<p class="mbottom">
+				00 77 66 55 33<br >
+				00 77 66 55 33</p>-->
+			<p class="mbottom">
+				<!--<a href="#">address@email.com</a><br >-->
+				<a href="https://www.facebook.com/gotasoft">GOTASOFT on Facebook</a><br >
+				<a href="https://twitter.com/gotasoft">@gotasoft on Twitter</a><br >				
+			</p>
+			<p>
+				<h6>Openning hours: </h6>
+				Monday to Friday <br >
+				09h00 to 17h00
+			</p>
+		
+		</aside>
+		
+		<section class="grid col-three-quarters mq2-col-two-thirds mq3-col-full">
+			<?
+				// redirect to success page 
+			if ($success){		
+			?>
+			<h2>TANKS! We will contacnt you back soon</h2>
+			<?
+			}
+			else{
+			?>			
+			<h2>ERROR! Please try again later</h2>
+			<?
+			}
+			?>
+			<!-- <p class="warning">GOTASOFT</p>
+			
+			<form id="contact_form" class="contact_form" action="contact.php" method="post" name="contact_form">	
+				<ul>
+					<li>
+						<label for="name">Your name:</label>
+						<input type="text" name="name" id="name" required class="required" >
+					</li>
+					<li>
+						<label for="email">Email:</label>
+						<input type="email" name="email" id="email" required placeholder="example@example.com" class="required email">
+					</li>	
+					<li>
+						<label for="message">Message:</label>
+						<textarea name="message" id="message" cols="100" rows="6" required  class="required" ></textarea>
+					</li>
+					<li>
+						<button type="submit" id="submit" name="submit" class="button fright">Send it</button>
+					</li>	
+				</ul>			
+			</form>-->
+		</section>	
+		
+	</div> <!--main-->
+
+<div class="divide-top">
+<footer class="grid-wrap">
+	<ul class="grid col-one-third social">
+		<li><a href="https://www.facebook.com/gotasoft">Facebook</a></li>
+			<li><a href="https://twitter.com/gotasoft">Twitter</a></li>
+	</ul>
+
+	<div class="up grid col-one-third ">
+		<a href="#navtop" title="Go back up">&uarr;</a>
+	</div>
+	
+	<nav class="grid col-one-third ">
+		<ul>
+			<li><a href="index.html">Home</a></li>
+			<li><a href="about.html">About</a></li>
+	<!--		<li><a href="works.html">Works</a></li>
+			<li><a href="services.html">Services</a></li>
+			<li><a href="blog.html">Blog</a></li>-->
+			<li><a href="contact.html">Contact</a></li>
+		</ul>
+	</nav>
+</footer>
+</div>
+
+</div>
+
+<!-- Javascript - jQuery -->
+<script src="http://code.jquery.com/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/jquery-1.7.2.min.js"><\/script>')</script>
+
+<!--[if (gte IE 6)&(lte IE 8)]>
+<script src="js/selectivizr.js"></script>
+<![endif]-->
+
+<script src="js/scripts.js"></script>
+<script src="js/jquery.validate.min.js"></script>
+
+<!-- Asynchronous Google Analytics snippet. Change UA-XXXXX-X to be your site's ID. -->
+<script>
+  var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+  (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+  g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+  s.parentNode.insertBefore(g,s)}(document,'script'));
+</script>
+</body>
+</html>
